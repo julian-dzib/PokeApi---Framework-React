5b67bbdeaf13a32ebb512e7f9e66a9e5fb46845e
dzibfer6 <6256@itescam.edu.mx>
2026-01-06 12:41:55 -0600

    se corrigió la vista principal y se agregó el buscador usando filter

--- pokeapp/app/page.tsx
+++ pokeapp/app/page.tsx
@@ -1,21 +1,45 @@
 "use client";
 import PokemonList from "@/components/pokemon_list/page";
 import { usePokemonList } from "@/hooks/pokemon_list_hook";
+import { useState } from "react";
 
 export default function Home() {
   // usar el hook que se creo
   const { pokemonsList, isLoading, page, setPage } = usePokemonList();
+  // buscador de pokemmons
+  const [search, setSearch ]= useState("");
+
+  //Definimos el filtrado de pokemones
+  const filteredPokemons = pokemonsList.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <p className="text-lg font-medium">Fernando Julian Puc Dzib</p>
+
+        <div className="flex-1 flex justify-center">
+          <input
+            type="text"
+            placeholder="Buscar Pokémon..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border rounded px-2 py-1 w-64"
+          />
+        </div>
+      </div>
+
       {isLoading ? (
         <p className="text-center">Cargando Pokemones...</p>
       ) : (
         <>
-          <PokemonList pokemons={pokemonsList} />
+        
+          <PokemonList pokemons={filteredPokemons}/>
 
-          {/* PAGINACIÓN */}
-          <div className="flex justify-center gap-4 mt-6">
+          <div className="flex justify-end gap-4 mt-4">
             <button
               onClick={() => setPage((p) => Math.max(p - 1, 1))}
               disabled={page === 1}
